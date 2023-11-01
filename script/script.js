7e69e08513e1fdc53eb655746400281e09c0391e
Muhammad Usman <usmansaleem4446996@gmail.com>
2023-11-01 09:55:23 +0500

    Parallex and Skill Scroll Effect Added

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
-    let textContainer = document.querySelector(".intro-text") 
-    let text = `Hi! My Name is  A Front-End Web Develepor.`;
+    let textContainer = document.querySelector(".intro-text")
+    let text = `Hi! My Name is   A Front-End Web Develepor.`;
     let index = 0;
     let lineBreakIndex = [3, 16];
     let spanIndex = [15];
@@ -17,11 +17,6 @@ window.addEventListener("load", () => {
                 let element = document.querySelector("#color");
                 let text = document.createTextNode("Muhammad Usman");
                 element.appendChild(text);
-                // let text = "Muhammad Usman";
-                // let now
-                // (if  == 15) {
-                //     element.innerHTML += text.charAt(index)
-                // }
             }
 
             textContainer.innerHTML += text.charAt(index);
@@ -29,6 +24,47 @@ window.addEventListener("load", () => {
             setTimeout(loop, 100);
         }
     }
+    setInterval(loop, 1000);
+});
 
-    loop();
+window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+
+    let container = document.querySelector("div.container");
+    container.style.top = value * 0.50 + "px";
+
+
+    let skillContainerHTML = document.querySelector(".skill-html");
+    let vertLine = document.querySelector(".vertical-line");
+    if (value > 400) {
+        skillContainerHTML.classList.add("skill-left-set");
+        vertLine.style.height = 20 + 'vh';
+    } else {
+        skillContainerHTML.classList.remove("skill-left-set");
+        vertLine.style.height = 0 + 'vh';
+    };
+
+    let skillContainerCSS = document.querySelector(".skill-css");
+    if (value > 550) {
+        skillContainerCSS.classList.add("skill-right-set");
+        vertLine.style.height = 40 + 'vh';
+    } else {
+        skillContainerCSS.classList.remove("skill-right-set");
+    };
+
+    let skillContainerJS = document.querySelector(".skill-js");
+    if (value > 700) {
+        skillContainerJS.classList.add("skill-left-set");
+        vertLine.style.height = 60 + 'vh';
+    } else {
+        skillContainerJS.classList.remove("skill-left-set");
+    };
+
+    let skillContainerGit = document.querySelector(".skill-git");
+    if (value > 850) {
+        skillContainerGit.classList.add("skill-right-set");
+        vertLine.style.height = 80 + 'vh';
+    } else {
+        skillContainerGit.classList.remove("skill-right-set");
+    };
 });
